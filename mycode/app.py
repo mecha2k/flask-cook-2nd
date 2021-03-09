5716150e308f39c1537df543926f73490b3f96c4
mecha2k <mecha2k@naver.com>
2021-03-09 21:57:28 +0900

    mycode

--- mycode/app.py
+++ mycode/app.py
@@ -1,28 +1,27 @@
 from flask import Flask
-from flask import Blueprint
+
+
+app = Flask(__name__)
+app.env = "development"
+
 
 MESSAGES = {
     "default": "Hello to the World of Flask!",
 }
-hello = Blueprint("hello", __name__)
-
-app = Flask(__name__)
-app.register_blueprint(hello)
-app.env = "development"
 
 
-@hello.route("/")
-@hello.route("/hello")
+@app.route("/")
+@app.route("/hello")
 def hello_world():
     return MESSAGES["default"]
 
 
-@hello.route("/show/<key>")
+@app.route("/show/<key>")
 def get_message(key):
     return MESSAGES.get(key) or "%s not found!" % key
 
 
-@hello.route("/add/<key>/<message>")
+@app.route("/add/<key>/<message>")
 def add_or_update_message(key, message):
     MESSAGES[key] = message
     return "%s Added/Updated" % key
